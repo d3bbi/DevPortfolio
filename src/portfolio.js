db83bf064e1ab3d75583178355f241647404fc75
Deborah <deborah_rimei@hotmail.com>
2020-12-06 14:04:11 +0000

    changed skills-bars and homepage description

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,8 @@ const greeting = {
   /* Your Summary And Greeting Section */
   username: "Deborah Rimei",
   title: "Deborah Rimei",
-  subTitle: emoji("I am a Computer Science student passionate in coding with one year of experience coding with Java and Javascript."),
+  subTitle: emoji("I am a second year BScH Computer Science student with hands-on experience building dynamic websites and project using object-oriented languages such as Java."+
+  " On my spare time I love to improve my coding skills and learn new technologies."),
   resumeLink: "https://drive.google.com/file/d/1m9AQn_wtUlwTvRcjv8iFGaS35dX-3d0z/view?usp=sharing"
 };
 
@@ -29,8 +30,8 @@ const skillsSection = {
   subTitle: emoji("WHAT I AM CURRENTLY FOCUSING ON"),
   skills: [
     emoji("👩‍💻 Develop user-friendly and responsive Front end / User Interfaces for the web and mobile"),
-    emoji("👩‍💻 Practising data structure and algorthms with Java"),
-    emoji("👩‍💻 Integration of third party services such as Firebase")
+    emoji("👩‍💻 Practicing data structure and algorthms with Java"),
+    emoji("👩‍💻 Learning to build android apps with Kotlin and Firebase")
   ],
 
   /* Make Sure You include correct Font Awesome Classname to view your icon
@@ -81,8 +82,8 @@ const educationInfo = {
       desc: "This BSc (Hons) is a 4-year program designed to equip students with a thorough understanding of the key principles of computer science.",
       PPA: "First year completed with PPA 85%.",
       descBullets: [
-        "Object Oriented Programming - Java: ",
-        "Web Development and Design - HTML/CSS, Javascript: ",
+        "Object Oriented Programming - Java",
+        "Web Development and Design - HTML/CSS, Javascript",
         "Relational Databases - SQL",
         "System Analysis and Design",
       ]
@@ -106,7 +107,16 @@ const techStack = {
     {
       Stack: "Javascript",
       progressPercentage: "65%"
+    },
+    {
+      Stack: "ReactJS",
+      progressPercentage: "60%"
+    },
+    {
+      Stack: "MySQL",
+      progressPercentage: "65%"
     }
+
   ]
 };
 
@@ -126,21 +136,7 @@ const workExperiences = {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
       ]
-    },
-    {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
+    }
   ]
 };
 
@@ -204,72 +200,10 @@ const achievementSection = {
         { name: "Award Letter", url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing" },
         { name: "Google Code-in Blog", url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html" }
       ]
-    },
-    {
-      title: "Google Assistant Action",
-      subtitle: "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
-      footerLink: [{ name: "View Google Assistant Action", url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en" }]
-    },
-
-    {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
-      footerLink: [
-        { name: "Certification", url: "" },
-        { name: "Final Project", url: "https://pakistan-olx-1.firebaseapp.com/" }
-      ]
     }
   ]
 };
 
-// Blogs Section
-
-const blogSection = {
-
-  title: "Blogs",
-  subtitle: "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
-
-  blogs: [
-    {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
-      description: "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
-    },
-    {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
-      description: "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
-    }
-  ]
-};
-
-// Talks Sections
-
-const talkSection = {
-  title: "TALKS",
-  subtitle: emoji("I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"),
-
-  talks: [
-    {
-      title: "Build Actions For Google Assistant",
-      subtitle: "Codelab at GDG DevFest Karachi 2019",
-      slides_url: "https://bit.ly/saadpasta-slides",
-      event_url: "https://www.facebook.com/events/2339906106275053/"
-    }
-  ]
-};
-
-// Podcast Section
-
-const podcastSection = {
-  title: emoji("Podcast 🎙️"),
-  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
-
-  // Please Provide with Your Podcast embeded Link
-  podcast: ["https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"]
-};
 
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
@@ -278,11 +212,4 @@ const contactInfo = {
   email_address: "deborah_rimei@hotmail.com"
 };
 
-//Twitter Section
-
-const twitterDetails = {
-
-  userName: "twitter"//Replace "twitter" with your twitter username without @
-
-};
-export { greeting, socialMediaLinks, skillsSection, educationInfo, techStack, workExperiences, openSource, bigProjects, achievementSection, blogSection, talkSection, podcastSection, contactInfo, twitterDetails };
+export { greeting, socialMediaLinks, skillsSection, educationInfo, techStack, workExperiences, openSource, bigProjects, achievementSection, contactInfo };
